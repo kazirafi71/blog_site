527c0cfddb31c6826a1bd33b9355176a9289001c
kazirafi71 <kazirafi668@gmail.com>
2021-07-06 01:49:33 +0600

    model and admin created

--- Backend/blog/admin.py
+++ Backend/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
---
+++ Backend/blog/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PosrSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    description = serializers.CharField(max_length=400)
+
+    def create(self, validated_data):
+        return Post.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get(
+            'description', instance.description)
